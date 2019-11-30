3a3cada70815ee1370a90461d6ce79af4b624a32
Ryan Harrington <rharrington.web@gmail.com>
2019-11-30 16:01:23 -0700

    derivatives setup

--- public/js/derivatives.js
+++ public/js/derivatives.js
@@ -10,3 +10,19 @@ __output__
 slope & f'(x)
 
 */
+
+// limit calculator
+
+/*
+
+    __Input__
+    x => ?
+    f(x) = ?
+
+    __Output__
+    limit value or DNE
+    
+    __Steps__
+    f'(x) = lim_[dx -> c] (f(c + dx) - f(c))/dx
+    
+*/
