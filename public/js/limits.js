acd43e8cc22ad4be9ae09ecd227e805044fb216e
Ryan Harrington <rharrington.web@gmail.com>
2019-12-04 19:29:11 -0700

    Bug fixes & replacements

--- public/js/limits.js
+++ public/js/limits.js
@@ -1,70 +1,58 @@
-// limit calculator
 
 /*
 
-    __Input__
-    x => ?
-    f(x) = ?
-
-    __Output__
-    limit value or DNE
-    
-    __Steps__
     1. f(x) is defined at c
     2. the limit exists at f(c)
     3. f(c) = lim_[x->c]f(c)
     
 */
 
+
+const replacements = [
+    ["^", "**"], 
+    ["sin(", "Math.sin("], 
+    ["cos(", "Math.cos("],
+    ["tan(", "Math.tan("],
+    ["arcsin(", "Math.asin("],
+    ["arccos(", "Math.acos("], 
+    ["arctan(", "Math.atan("]
+]
+
+// JavaScript Math does not support trigonometric functions such as
+// csc(x), cot(x), arcsec(x), etc.
+
 const f = (func, x) => {
-
-
-    func = func.replace(/\^/g, "**");
-    func = func.replace(/\(/g, "*(");
-    func = func.replace(/\/\*\(/g, "/\(");
-    func = func.replace(/x/g, String(x));
-    func = func.replace(/cos\(x\)/g, Math.cos(x));
-    func = func.replace(/sin(x)/g, Math.sin(x));
-    func = func.replace(/tan(x)/g, Math.tan(x));
-    // Support added for trigonometric functions except inverse trigonometric functions.
-    // cleanup? 
-    // More replaces functions may be needed.
-
-    if (eval(func))
-        return eval(func);
     
-    else return "DNE";
 
-}
+    for (l in replacements) {
+        func = func.replace(replacements[l][0], replacements[l][1]);
+    }
 
-
-const evaluate = (func, c) => {
-
-    msg = "Calculating <b>lim_[x -> " + c + "] " + func + "</b>... ";
-    $("#result").html(msg);
-    
-    const s1 = f(func, c);
-    var s2 = "DNE";
-
-    limR = Math.round(f(func, c + 0.01));
-    limL = Math.round(f(func, c - 0.01));
-
-    if (limR === limL)
-        s2 = limR;
-
-    if (s1 == s2)
-        $("#result").html(msg + s1);
-
-    else $("#result").html(msg + "DNE");
+    func = func.replace(/x/g, String(x));    
+    return eval(func);
 
 }
 
 $("#calc").click(() => {
-
+    
     const c = parseFloat($("#limit-x").val());
-    const f = $("#limit-f").val();
-
-    evaluate(f.replace(/\s+/g, ' '), c);
+    const func = $("#limit-f").val();
+    
+    msg = `Calculating <b>lim_[x -> ${c}] ${func}</b>... `;
+    $("#result").html(msg);
+    
+    const s1 = f(func, c);
+    var s2 = "DNE";
+    limR = Math.round(f(func, c + 0.0001));
+    limL = Math.round(f(func, c - 0.0001));
+    
+    if (limR == limL)
+        s2 = limR;
+    
+    if (s1 == s2)
+        $("#result").html(`${msg}<h1>${s1}</h1>`);
+    
+    else $("#result").html(`${msg}<h1>DNE</h1>`);
 
 });
 
