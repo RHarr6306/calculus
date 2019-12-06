7d81612315f9851c7563a8c6212e26a636de3866
Ryan Harrington <rharrington.web@gmail.com>
2019-12-05 19:42:55 -0700

    styling

--- public/js/limits.js
+++ public/js/limits.js
@@ -37,17 +37,17 @@ const lim = (func, c) => {
 
     const s1 = f(func, c);
     var s2 = "DNE";
-    limR = Math.round(f(func, c + 0.0001));
-    limL = Math.round(f(func, c - 0.0001));
+    limR = Math.round(f(func, c + 0.000001) * 100) / 100;
+    limL = Math.round(f(func, c - 0.000001) * 100) / 100;
     
     if (limR == limL)
         s2 = limR;
     
     if (s1 == s2)
         return `${msg}<h1>${s1}</h1>`;
-    
+        
     else return `${msg}<h1>DNE</h1>`;
-
+        
 }
 
 $("#calc").click(() => {
